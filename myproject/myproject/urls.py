94f0e1cb8fa27caba1e8e3f8e6b1684b761cfb51
renhorikawa <renhoript@gmail.com>
2024-11-30 17:21:01 +0900

    gg

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     # 他のパス設定
+    path('', views.index, name='index'),  # ルート URL に対するビューを追加
     path('upload/', views.upload_video, name='upload_video'),
     path('result/', views.result, name='result'),
     # その他のURLパターン

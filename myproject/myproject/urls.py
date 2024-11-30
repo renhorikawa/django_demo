2c6417ea6688cd609e61de3c6864d1dc13693847
renhorikawa <renhoript@gmail.com>
2024-11-30 16:51:36 +0900

    g

---
+++ myproject/myproject/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path  # pathをインポート
+from django.conf import settings
+from django.conf.urls.static import static  # staticをインポート
+from . import views
+
+urlpatterns = [
+    # 他のパス設定
+    path('upload/', views.upload_video, name='upload_video'),
+    path('result/', views.result, name='result'),
+    # その他のURLパターン
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
